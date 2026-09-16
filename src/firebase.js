dd133753adf53142054c34da442224e6ef29bf6b
BHARATFLIX <satyveer45singh@gmail.com>
2026-09-16 16:58:30 +0530

    Update Firebase API key and formatting

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHf6yK7J8L9M0N1O2P3Q4R5S6T7U8V9W0",
+  apiKey: "AIzaSyCCevL_8GSXw0S7u9ujcrQJmiTfpTn87qE",
   authDomain: "bharatflix-82578.firebaseapp.com",
   projectId: "bharatflix-82578",
   storageBucket: "bharatflix-82578.firebasestorage.app",
@@ -10,6 +10,7 @@ const firebaseConfig = {
   appId: "1:858704386202:web:9cfd17f85b3593d5516723"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
